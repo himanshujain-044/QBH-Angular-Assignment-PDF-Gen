5a663f62ff448ed5052294e55f168bac3f6d3ae1
Himanshu Jain <himanshujain044@gmail.com>
2024-06-16 16:48:18 +0530

    fix: small changes, removed unused code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CComponent } from './components/c/c.component';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, CComponent],
-  providers: [BrowserModule, FormsModule, CommonModule],
+  providers: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'HBQ Assignment';
-  
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,7 @@ import {
   provideHttpClient,
   withInterceptorsFromDi,
 } from '@angular/common/http';
-import { provideAnimations } from '@angular/platform-browser/animations';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
